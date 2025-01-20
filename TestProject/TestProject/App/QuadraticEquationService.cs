af1977bba8a49e2816888a5d7f079d07cba2f849
Bogdan <bnechaenko@sigma-it.ru>
2025-01-20 13:59:04 +0300

    create test project, first two test cases

---
+++ TestProject/TestProject/App/QuadraticEquationService.cs
@@ -0,0 +1,24 @@
+﻿namespace TestProject.App
+{
+    public class QuadraticEquationService
+    {
+        public double[] Solve(double a, double b, double c)
+        {
+            double discr = Math.Pow(b, 2) - 4 * a * c;
+            double[] roots = new double[2];
+
+            if (discr < 0)
+            {
+                return new double[0];
+            }
+            else if (discr > 0)
+            {
+                var root1 = (-b + Math.Sqrt(discr)) / 2 * a;
+                var root2 = (-b - Math.Sqrt(discr)) / 2 * a;
+                return [root1, root2];
+            }
+
+            return roots;
+        }
+    }
+}
---
+++ TestProject/TestProject/Tests/Test1.test.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using TestProject.App;
+
+namespace TestProject.Tests
+{
+    [TestFixture]
+    public class Test1
+    {
+        private QuadraticEquationService _quadraticEquationService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _quadraticEquationService = new QuadraticEquationService();
+        }
+
+        [Test]
+        public void DiscriminantNegative_NoRoots()
+        {
+            double[] result = _quadraticEquationService.Solve(1, 0, 1);
+
+            Assert.That(result, Is.Empty, "no roots");
+        }
+
+        [Test]
+        public void DiscriminantPositive_TwoRoots()
+        {
+            double[] result = _quadraticEquationService.Solve(1, 0, -1);
+            double[] expected = [1, -1];
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
