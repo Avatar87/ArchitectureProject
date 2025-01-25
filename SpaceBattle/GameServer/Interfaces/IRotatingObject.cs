8da3add490f120e3a507b2af6e92fc3f93cdbe79
Bogdan <bnechaenko@sigma-it.ru>
2025-01-25 20:01:09 +0300

    add spacebattle solution, models, interfaces and tests for move and rotate

---
+++ SpaceBattle/GameServer/Interfaces/IRotatingObject.cs
@@ -0,0 +1,11 @@
+﻿using GameServer.Enums;
+
+namespace GameServer.Interfaces
+{
+    public interface IRotatingObject
+    {
+        public Position GetPosition();
+
+        public void SetPosition(Position newPosition);
+    }
+}
---
+++ SpaceBattle/GameServer/Models/SpaceShip.cs
@@ -0,0 +1,43 @@
+﻿using GameServer.Enums;
+using GameServer.Interfaces;
+
+namespace GameServer.Models
+{
+    public class SpaceShip : IMovingObject, IRotatingObject
+    {
+        private Point _location { get; set; }
+        private Vector _velocity { get; set; }
+        private Position _position { get; set; }
+
+        public SpaceShip(Point location, Vector velocity, Position position)
+        {
+            _location = location;
+            _velocity = velocity;
+            _position = position;
+        }
+
+        public Point GetLocation()
+        {
+            return _location;
+        }
+        public void SetLocation(Point newLocation)
+        {
+            _location = newLocation;
+        }
+
+        public Vector GetVelocity()
+        {
+            return _velocity;
+        }
+
+        public Position GetPosition()
+        {
+            return _position;
+        }
+
+        public void SetPosition(Position newPosition)
+        {
+            _position = newPosition;
+        }
+    }
+}
