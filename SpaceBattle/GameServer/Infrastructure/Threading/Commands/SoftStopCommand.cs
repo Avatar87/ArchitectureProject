b4286de3db5cc1448279d7ea73f1af4cc52362fc
Bogdan <bnechaenko@sigma-it.ru>
2025-03-12 22:03:44 +0300

    add server thread class, stop commands and tests

---
+++ SpaceBattle/GameServer/Infrastructure/Threading/Commands/SoftStopCommand.cs
@@ -0,0 +1,30 @@
+﻿
+using GameServer.Interfaces;
+
+namespace GameServer.Infrastructure.Threading.Commands
+{
+    public class SoftStopCommand : ICommand
+    {
+        private ServerThread _st;
+        public SoftStopCommand(ServerThread st)
+        {
+            _st = st;
+        }
+
+        public void Execute()
+        {
+            Action oldBehaviour = _st.behaviour;
+            _st.SetBehaviour(() =>
+            {
+                if (_st.q.Count > 0)
+                {
+                    oldBehaviour();
+                }
+                else
+                {
+                    _st.Stop();
+                }
+            });
+        }
+    }
+}
