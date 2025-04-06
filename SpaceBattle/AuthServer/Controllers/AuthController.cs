e5f1a3a01c5f7cfc8b68062f4da9b758351c273c
Bogdan <bnechaenko@sigma-it.ru>
2025-04-06 22:54:40 +0300

    add AuthServer and jwt authentication

---
+++ SpaceBattle/AuthServer/Controllers/AuthController.cs
@@ -0,0 +1,97 @@
+﻿using AuthServer.Models;
+using AuthServer.Providers;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthServer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AccountController : ControllerBase
+    {
+        #region Private fields
+
+        private readonly JwtTokenProvider _tokenProvider;
+        private readonly UserManager<User> _userManager;
+
+        private static List<User> users = new()
+        {
+            new User { UserName = "Petya", Password = "123", IsAdmin = true, IsPrivileged = true },
+            new User { UserName = "Vasya", Password = "12345", IsPrivileged = true },
+            new User { UserName = "Gosha", Password = "qwerty123" },
+        };
+
+        private static List<Game> games = new()
+        {
+            new Game { Id = Guid.Parse("29a425d1-84a7-45c4-96c6-7d154c13aa94"), Users = new List<User> { } },
+            new Game { Id = Guid.Parse("75edd8af-e261-4694-be39-bd3c81b22a2b"), Users = users },
+            new Game { Id = Guid.Parse("1aeaf07c-2272-4c41-865c-175c0687d375"), Users = new List<User> { new User { UserName = "Fedya", Password = "qwerty" } } },
+        };
+
+        #endregion
+
+        #region Constructor
+
+        public AccountController(JwtTokenProvider tokenProvider, UserManager<User> userManager)
+        {
+            _tokenProvider = tokenProvider;
+            _userManager = userManager;
+        }
+
+        #endregion
+
+        #region Public methods
+
+        [HttpPost]
+        [Route("WebClient/CreateBattle")]
+        public ActionResult<Guid> CreateBattle(IEnumerable<User> players)
+        {
+            Game game = new()
+            {
+                Id = Guid.NewGuid(),
+                Users = players
+            };
+            games.Add(game);
+            return Ok(game.Id);
+        }
+
+        [HttpPost]
+        [Route("WebClient/Login")]
+        public async Task<ActionResult<string>> Authenticate(Guid gameId, string userName, string password)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(password) || string.IsNullOrWhiteSpace(userName))
+                {
+                    throw new Exception("Имя пользователя или пароль не указаны");
+                }
+
+                var trimmedLogin = userName.Trim();
+
+                User? appUser = users.Find(u => u.UserName == userName && u.Password == password);
+
+                if (appUser == null)
+                {
+                    throw new Exception("Неправильное имя пользователя или пароль");
+                }
+
+                Game? game = games.Find(g => g.Id == gameId);
+
+                if (game == null)
+                {
+                    throw new Exception($"Игра с идентификатором {gameId} не найдена");
+                }
+
+                string token = await _tokenProvider.GenerateAsync("Game", _userManager, appUser, gameId);
+
+                return Ok(token);
+            }
+            catch (Exception e)
+            {
+                return Unauthorized();
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ SpaceBattle/AuthServer/Providers/JwtTokenProvider.cs
@@ -0,0 +1,118 @@
+﻿using AuthServer.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace AuthServer.Providers
+{
+    public class JwtTokenProvider
+    {
+        private readonly string _tokenType;
+        private readonly string _issuer;
+        private readonly string _audience;
+        private readonly int _tokenLifetime;
+        private readonly SymmetricSecurityKey _securityKey;
+
+        protected JwtTokenProvider(
+            string tokenType,
+            string key,
+            string issuer,
+            string audience,
+            int tokenLifetime)
+        {
+            _tokenType = tokenType;
+            _issuer = issuer;
+            _audience = audience;
+            _tokenLifetime = tokenLifetime;
+            _securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+        }
+
+        public async Task<string> GenerateAsync(string purpose, UserManager<User> manager, User user, Guid gameId)
+        {
+            var creds = new SigningCredentials(_securityKey, SecurityAlgorithms.HmacSha256Signature);
+
+            var roles = await manager.GetRolesAsync(user);
+            var claims = await manager.GetClaimsAsync(user);
+            claims.Add(new Claim(ClaimTypes.NameIdentifier, user.UserName));
+            claims.Add(new Claim(ClaimTypes.Name, user.UserName));
+            foreach (var role in roles) { claims.Add(new Claim(ClaimTypes.Role, role)); }
+
+            var accessToken = CreateToken
+            (
+                creds,
+                _issuer,
+                _audience,
+                claims,
+                DateTime.UtcNow.AddMinutes(_tokenLifetime)
+            );
+
+            accessToken.SigningKey = _securityKey;
+            accessToken.Payload["UserName"] = user.UserName;
+            accessToken.Payload["GameId"] = gameId;
+
+            return await Task.FromResult(new JwtSecurityTokenHandler().WriteToken(accessToken));
+        }
+
+        public Task<bool> ValidateAsync(string token)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            if (!tokenHandler.CanReadToken(token))
+            {
+                return Task.FromResult(false);
+            }
+
+            try
+            {
+                ValidateTokenWithParams(token);
+                return Task.FromResult(true);
+            }
+            catch (SecurityTokenExpiredException exception)
+            {
+                throw;
+            }
+            catch (SecurityTokenException e)
+            {
+                return Task.FromResult(false);
+            }
+        }
+
+        protected JwtSecurityToken CreateToken(SigningCredentials credentials, string issuer, string audience, IEnumerable<Claim> claims, DateTime expires)
+        {
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+
+            return handler.CreateJwtSecurityToken(
+                issuer: issuer,
+                audience: audience,
+                subject: new ClaimsIdentity(claims),
+                notBefore: DateTime.UtcNow,
+                expires: expires,
+                signingCredentials: credentials
+            );
+        }
+
+
+        private SecurityToken ValidateTokenWithParams(string token)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            tokenHandler.ValidateToken(token,
+                new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    IssuerSigningKey = _securityKey,
+                    ValidIssuer = _issuer,
+                    ValidAudience = _audience,
+                    ClockSkew = TimeSpan.FromSeconds(5)
+                },
+                out SecurityToken validatedToken);
+
+            return validatedToken;
+        }
+    }
+}
